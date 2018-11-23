74deacd6c7e1fc7596afc08cd56b500eb0ae98ad
Marco <hawlett74@yopmail.com>
2018-11-23 14:54:37 +0000

    prova su gradi

---
+++ celsiusform.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Temperature Conversion</title></head>
+<body>
+<?php $fahrenheit = $_POST['fahrenheit']; ?>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+Fahrenheit temperature:
+<input type="text" name="fahrenheit" value="<?php echo $fahrenheit; ?>"/><br />
+<input type="submit" value="Convert to Celsius!" />
+</form>
+<?php 
+if (!is_null($fahrenheit)) {
+//$fahrenheit = $_POST['fahrenheit'];
+$celsius = ($fahrenheit - 32) * 5 / 9;
+printf("%.2fF is %.2fC", $fahrenheit, $celsius);
+//echo $celsius;
+}
+ ?>
+</body>
+</html>
